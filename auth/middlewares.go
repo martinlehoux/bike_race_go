dea789a5ffdfdf72f210880e313bf31403633ad0
Martin Kagamino Lehoux <martin@lehoux.net>
2023-05-07 18:58:46 +0200

    fix: Remove basic auth

--- auth/middlewares.go
+++ auth/middlewares.go
@@ -14,27 +14,8 @@ import (
 )
 
 func Unauthorized(w http.ResponseWriter, err error) {
-	w.Header().Add("WWW-Authenticate", `Basic realm="private"`)
 	w.WriteHeader(http.StatusUnauthorized)
 	w.Write([]byte(err.Error()))
-}
-
-func BasicAuthMiddleware(conn *pgx.Conn) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
-			username, password, ok := r.BasicAuth()
-			if ok {
-				user, err := AuthenticateUser(ctx, conn, username, password)
-				if err != nil {
-					Unauthorized(w, err)
-					return
-				}
-				ctx = context.WithValue(ctx, "user", user)
-			}
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
 }
 
 func CookieAuthMiddleware(conn *pgx.Conn, secret []byte) func(http.Handler) http.Handler {
