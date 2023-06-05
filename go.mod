16c7041aa1994aa30ed46cb765ae547c8d5b7c80
Martin Kagamino Lehoux <martin@lehoux.net>
2023-06-05 23:32:12 +0200

    chore: Extract locales from templates

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	golang.org/x/crypto v0.8.0
 	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc
 	golang.org/x/tools v0.9.1
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -31,6 +32,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
 	go.opentelemetry.io/otel/trace v1.16.0 // indirect
@@ -44,5 +46,4 @@ require (
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ tools/gettext/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bike_race/core"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"golang.org/x/exp/slog"
+	"gopkg.in/yaml.v3"
+)
+
+func extractKeys() map[string]int {
+	extractedKeys := make(map[string]int, 0)
+
+	reg, err := regexp.Compile(`{{\s*call \$?\.T\s*"(.+)"\s*(?:([.\w]+)\s*)*}}`)
+	core.Expect(err, "error compiling regexp")
+
+	filepath.Walk("templates", func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() && filepath.Ext(path) == ".html" {
+			content, err := os.ReadFile(path)
+			core.Expect(err, "error reading file")
+			matches := reg.FindAllStringSubmatch(string(content), -1)
+			for _, match := range matches {
+				key := match[1]
+				parts := strings.Split(strings.TrimSpace(match[0][2:len(match[0])-2]), " ")
+				extractedKeys[key] = len(parts) - 3
+			}
+		}
+		return nil
+	})
+
+	return extractedKeys
+}
+
+func main() {
+	baseLogger := slog.Default()
+	langs := [...]string{"en-GB", "fr-FR"}
+
+	extractedKeys := extractKeys()
+	baseLogger.Info("extracted keys from templates", slog.Int("count", len(extractedKeys)))
+
+	for _, lang := range langs {
+		logger := baseLogger.With(slog.String("lang", lang))
+		currentLocales := make(map[string]string, 0)
+		newLocales := make(map[string]string, 0)
+
+		locales, err := os.ReadFile(filepath.Join("locales", lang, "index.yml"))
+		core.Expect(err, "error reading file")
+		core.Expect(yaml.Unmarshal(locales, &currentLocales), "error unmarshalling yaml")
+
+		correctLocales := 0
+		for key, translation := range currentLocales {
+			currentArgsCount := strings.Count(translation, "%")
+			expectedArgsCount, ok := extractedKeys[key]
+			if !ok {
+				logger.Info(`found unused key`, slog.String("key", key))
+			} else if currentArgsCount != extractedKeys[key] {
+				logger.Info(`found translation with incorrect number of arguments`, slog.String("key", key), slog.Int("current", currentArgsCount), slog.Int("expected", expectedArgsCount))
+				newLocales[key] = ""
+			} else if translation == "" {
+				newLocales[key] = ""
+			} else {
+				newLocales[key] = currentLocales[key]
+				correctLocales++
+			}
+		}
+
+		for key := range extractedKeys {
+			if _, ok := currentLocales[key]; !ok {
+				logger.Info(`found missing key`, slog.String("key", key))
+				newLocales[key] = ""
+			}
+		}
+
+		logger.Info("finished checking locales", slog.Int("count", len(newLocales)), slog.Int("correct", correctLocales), slog.String("completion", fmt.Sprintf("%d%%", correctLocales*100/len(extractedKeys))))
+
+		content, err := yaml.Marshal(newLocales)
+		core.Expect(err, "error marshalling yaml")
+		core.Expect(os.WriteFile(filepath.Join("locales", lang, "index.yml"), content, 0644), "error writing file")
+	}
+}
